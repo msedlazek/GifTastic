7758ce589cccf719a602956c2710a2b0ae8c3383
msedlazek <mark.sedlazek@gmail.com>
2016-12-13 21:09:18 -0500

    Got submit to add value to array and make a new button successfully.

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,52 @@
 $("document").ready(function(){
 
-	$(".btn").on("click", function(){
-		alert("hi");
+	// This first part will happen on page load, it appends the array as buttons to the page.
+
+	var topics = ["Guns", "Fantasy", "Cyberpunk"]
+	
+	makeButtons();
+
+	function makeButtons(){
+		for (var i = 0; i < topics.length; i++) {
+			var newBtn = $("<button>");
+			newBtn.text(topics[i]);
+			newBtn.val(topics[i]);
+			$("#topicButtons").append(newBtn);
+		};
+	};
+
+	function newTopics(){
+		$("#topicButtons").empty();
+	}
+
+	$("#addTopic").on("click", function(e){
+		var addedTopic = $("#topic-input").val();
+		topics.push(addedTopic);
+		newTopics();
+		makeButtons();
+		e.preventDefault();
 	});
+	
+	// $("button").on("click", function(){
+	// 	var searchTopic = $(this).val();
+	// 	var queryUrl = "http://api.giphy.com/v1/gifs/search?q=" + searchTopic + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+	// 	$.ajax({
+ //        	url: queryURL,
+ //        	method: "GET"
+ //      	}).done(function(response) {
+ //      		console.log(response);
+ //      	})
+
+	// });
+
+
+
+	// $("input").on("click", function(){
+	// 	alert("hi");
+	// });
 
 })
+
+// rating???
+// if (rating == g )
